99f6234a12d6e8bc0be6b738b71fc3d023909be3
Nikola Vlaskalin <nikola.vlaskalin00@gmail.com>
2026-03-10 22:11:39 +0100

    removed bracket..

--- task1/computing/variables.tf
+++ task1/computing/variables.tf
@@ -6,9 +6,5 @@ variable "region" {
 }
 
 variable "test" {
-<<<<<<< HEAD
   description = "Test variable 123"
-=======
-  description = "Test variable"
->>>>>>> 5274a0d532cb78e7161cc2c55ab76119a32a3d5c
 }
