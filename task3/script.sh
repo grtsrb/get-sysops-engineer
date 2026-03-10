27e33a24eccb0abafd5b508a03ab2f5c2283e45e
Nikola Vlaskalin <nikola.vlaskalin00@gmail.com>
2026-03-10 13:53:47 +0100

    Bash script finished

--- task3/script.sh
+++ task3/script.sh
@@ -7,7 +7,7 @@ if [[ ! -f "$file" ]]; then
   exit 1
 fi
 
-echo "====== 1. Kreiranje fajlova iz $file ======"
+echo "====== 1. Creating files from $file ======"
 while IFS= read -r line || [ -n "$line" ]; do
   filename=$(echo "$line" | tr -d '\r'| xargs)
   
@@ -22,7 +22,7 @@ if [[ $? -ne 0 ]]; then
   exit 0
 fi
 
-echo "====== 2. Checking format and sorting ======"
+echo "====== 2. Checking format and files sorting in correct directories ======"
 
 regex="^k([0-9a-fA-F]{8})\.kod$"
 
@@ -31,7 +31,7 @@ for f in *.kod; do
 
     hex_value="${BASH_REMATCH[1]}"
     # k A B C D E F G H . k o d
-    #   0 1 2 3 4 5 6 7 (hex_part indexi)
+    #   0 1 2 3 4 5 6 7 (hex_part index)
     
     E_digit="${hex_value:4:1}"
     G_digit="${hex_value:6:1}"
@@ -40,11 +40,12 @@ for f in *.kod; do
 
     if (( G_decimal % 2 == 0 )); then
       echo "$f - G digit is even (decimal: $G_decimal)"
-      target_dir="${E_digit}0/${G_digit}0"
+      target_dir="${G_digit}0/${E_digit}0"
     else
       echo "$f - G digit is odd (decimal: $G_decimal)"
       X_decimal=$((G_decimal - 1))
-      target_dir="${X_decimal}0/${E_digit}0"
+      X_digit=$(printf "%x" $X_decimal)
+      target_dir="${X_digit}0/${E_digit}0"
     fi
 
     mkdir -p "$target_dir"
